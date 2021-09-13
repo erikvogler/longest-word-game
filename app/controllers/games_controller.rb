9f1afd1f92e9bcb8070ade6066472f1bbb8de00e
Erik Vogler <erik.freetree@gmail.com>
2021-09-14 00:13:22 +0200

    added scroe function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,32 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
-    # binding.pry
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+
+    binding.pry
+
   end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    results = [ json['found'], json['lenght'] ]
+    results
+  end
+
+
 end
